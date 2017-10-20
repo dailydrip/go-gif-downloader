68b2072b710e28c23f34c72da9e89ec816082b1f
Luiz Filho <luizbafilho@gmail.com>
2017-10-19 21:43:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gif Downloader
+
+Gif downloader is a Go program to download gifs from Giphy.
+
+## Run
+```
+$ go build -o gif-downloader
+$ API_KEY=<your-giphy-api-key> ./gif-downloader -dir /tmp/gifs -limit 5 cats
+```
---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+)
+
+type Gif struct {
+	ID     string `json:"id"`
+	Slug   string `json:"slug"`
+	Images struct {
+		Original struct {
+			Url string `json:"url"`
+		} `json:"original"`
+	} `json:"images"`
+}
+
+type SearchResult struct {
+	Data       []Gif `json:"data"`
+	Pagination struct {
+		TotalCount int `json:"total_count"`
+	} `json:"pagination"`
+}
+
+type GiphyService struct {
+	BaseUrl string
+	APIKey  string
+	Dir     string
+}
+
+func (g GiphyService) Search(query string, limit int) ([]Gif, error) {
+	gifs := []Gif{}
+
+	url := fmt.Sprintf("%s/gifs/search?api_key=%s&q=%s&limit=%d", g.BaseUrl, g.APIKey, query, limit)
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	searchResult := SearchResult{}
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&searchResult)
+	if err != nil {
+		return gifs, err
+	}
+
+	return searchResult.Data, nil
+}
+
+func (g GiphyService) Download(gifs []Gif) {
+	fmt.Printf("Downloading gifs to: %s\n", g.Dir)
+	var wg sync.WaitGroup
+	for _, gif := range gifs {
+		wg.Add(1)
+		go func(gif Gif) {
+			defer wg.Done()
+
+			output, err := os.Create(fmt.Sprintf("%s/%s.gif", g.Dir, gif.Slug))
+			if err != nil {
+				fmt.Println("Error while creating gif file:", gif.Slug, err)
+				return
+			}
+			defer output.Close()
+
+			url := gif.Images.Original.Url
+			fmt.Printf("Downloading %s...\n", url)
+			resp, err := http.Get(url)
+			if err != nil {
+				fmt.Println("Error while downloading", url, "-", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			_, err = io.Copy(output, resp.Body)
+			if err != nil {
+				fmt.Println("Error while downloading", url, "-", err)
+			}
+		}(gif)
+	}
+
+	wg.Wait()
+	fmt.Println("Download done!")
+}
+
+func main() {
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == "" {
+		log.Fatal("API_KEY env. variable is required")
+	}
+
+	var limit int
+	var dir string
+	flag.IntVar(&limit, "limit", 5, "maximum number of gifs to download")
+	flag.StringVar(&dir, "dir", "/tmp/gifs", "gifs download path")
+	flag.Parse()
+
+	gs := GiphyService{
+		BaseUrl: "http://api.giphy.com/v1",
+		APIKey:  apiKey,
+		Dir:     dir,
+	}
+
+	if len(flag.Args()) == 0 {
+		log.Fatal("search query is required")
+	}
+
+	query := flag.Args()[0]
+
+	gifs, err := gs.Search(query, limit)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gs.Download(gifs)
+}
